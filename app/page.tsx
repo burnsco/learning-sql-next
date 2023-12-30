47bbd997427a81cd98d53568f3eec026d72be634
Corey Burns <coreyburns@outlook.com>
2023-12-30 12:50:05 -0500

    added bun back, faster

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
-import { scrape } from "@/app/lib/data";
-
 export default async function Home() {
-  const data = await scrape();
+  // const data = await scrape();
 
-  // console.log(data);
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Lets go</h1>
+      <h1 className="text-red-500 font-semibold">Lets go</h1>
+      <p>What else can you say?</p>
+      <div>
+        <li>testing testing 1 2 3</li>
+      </div>
+      <p>Hello this is a paragraph</p>
     </main>
   );
 }
