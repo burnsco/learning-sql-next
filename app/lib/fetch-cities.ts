31077009a530f89db7b8c3b0430a01d7c88b81bf
Corey Burns <coreyburns@outlook.com>
2024-01-02 14:31:43 -0500

    added a dynamic column to sql (pop density) + fixed table (needed to return explicity in the map in order for the key to pick up?

--- app/lib/fetch-cities.ts
+++ app/lib/fetch-cities.ts
@@ -1,15 +1,10 @@
 import * as db from '../../scripts/db.js';
 
-type Cities = {
-  id: string;
-  name: string;
-  population: number;
-  area: number;
-}[];
-
 export async function fetchCities() {
   try {
-    const data = await db.query(`SELECT * FROM cities`);
+    const textQuery = `
+    SELECT name, country, population, area, population / area AS density FROM cities`;
+    const data = await db.query(textQuery);
     return data.rows;
   } catch (error) {
     console.error('Database Error:', error);
